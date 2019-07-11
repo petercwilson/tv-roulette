9aebe20453108514a8280f6e156096f2c8ca5d8b
Peter Wilson <pcwilson19@gmail.com>
2019-07-11 17:31:48 -0500

    updated

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const searchURL = 'https://api.tvmaze.com/shows';
-const youTubeURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyDFIlqkeyIwWwdg3tvu8sgPbp63Pl_BmrA'
 
 function displayResults(responseJson) {
   console.log(responseJson);
   $('.fetchBtn').click(function(event) {
   $('.results').empty();
   let random = responseJson[Math.floor(Math.random() * responseJson.length)];
-  console.log(random.name)
     $('.results').append(`
       <img src ="${random.image.medium}" />
       <p>Title: ${random.name}</p>
@@ -23,6 +21,18 @@ function displayResults(responseJson) {
 };
 
   fetch(searchURL)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    }) 
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+
+  fetch(youTubeURL)
     .then(response => {
       if (response.ok) {
         return response.json();
