aac777b10fd59842e2fb823edd3cee2c07599b1f
Peter Wilson <pcwilson19@gmail.com>
2019-08-07 11:24:36 -0700

    fixed mobile versions

--- index.js
+++ index.js
@@ -35,6 +35,36 @@ function displayResults(responseJson) {
   });
 };
 
+function fetchAnotherShow(responseJson) {
+  $('#add-buttons').click(function(event) {
+    $('.results').empty();
+  const random = responseJson.results[Math.floor(Math.random() * responseJson.results.length)];
+    $('.results').append(`
+      <img class="thumbnail" src="https://image.tmdb.org/t/p/original${random.poster_path}" />
+      <div id="overview">
+        <h2>Overview</h2>
+        <p>${random.overview}</p>
+      </div>
+      <div id="add-buttons">
+        <button class="add-btn btn-red btn-large" role="link">fetch tv show</button>
+        <button class="btn btn-red btn-large video-btn" role="link">Watch Trailer</button>
+      </div>
+    `)
+  $('.results').removeClass('hidden');
+  $('.video-btn').click(function(event) {
+    $('.results').append(`
+      <div class="video-container">
+        <iframe id="ytplayer" type="text/html" width="1200" height="800"
+        src="https://www.youtube.com/embed/?listType=search&list=${random.name} trailer"
+        frameborder="0" allowfullscreen>
+        </iframe>
+        <button class="close" role="link">X</button>
+      </div>
+    `)
+    });
+  });
+};
+
 function closeButton(event) { 
   $('.results').on('click', '.close', function(event) {
     $('.video-container').fadeOut('fast');
@@ -53,4 +83,5 @@ fetch(popularURL)
     $('#js-error-message').text(`Something went wrong: ${err.message}`);
   });
 
+$(fetchAnotherShow);
 $(closeButton);
