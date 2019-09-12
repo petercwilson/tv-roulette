ccbdbc6aa62bd8eb98934e089dd9e426d3ffb49f
Peter Wilson <pcwilson19@gmail.com>
2019-09-12 08:57:28 -0700

    refactor functions

--- index.js
+++ index.js
@@ -1,69 +1,52 @@
 'use strict';
 
-const popularURL = 'https://api.themoviedb.org/3/tv/top_rated?api_key=56d2a76f60029e3b5159c82ad83f7df4&language=en-US&origin_country=us'
+const url = 'https://api.themoviedb.org/3/tv/top_rated?api_key=56d2a76f60029e3b5159c82ad83f7df4&language=en-US&origin_country=us'
 
+function doStartUpStuff() {
+    $('.results').empty();
+}
 
 function displayResults(responseJson) {
-  $('.btn').click(function(event) {
-    $('.start').hide();
-    $('.results').empty();
-  const random = responseJson.results[Math.floor(Math.random() * responseJson.results.length)];
-  console.log(`"https://www.youtube.com/embed?listType=search&list=${random.name}"`)
-    $('.results').append(`
-      <img class="thumbnail" src="https://image.tmdb.org/t/p/original${random.poster_path}" />
-      <div id="overview">
-        <h2>Overview</h2>
-        <p>${random.overview}</p>
-      </div>
-      <div id="add-buttons">
-        <button class="add-btn btn-red btn-large" role="link">fetch tv show</button>
-        <button class="btn btn-red btn-large video-btn" role="link">Watch Trailer</button>
-      </div>
-    `)
-  $('.results').removeClass('hidden');
-  $('.video-btn').click(function(event) {
-    $('.results').append(`
-      <div class="video-container">
-        <iframe id="ytplayer" type="text/html" width="1200" height="800"
-        src="https://www.youtube.com/embed/?listType=search&list=${random.name} trailer"
-        frameborder="0" allowfullscreen>
-        </iframe>
-        <button class="close" role="link">X</button>
-      </div>
-    `)
-    });
-  });
+    doStartUpStuff();
+    getMovies(responseJson);
 };
 
-function fetchAnotherShow(responseJson) {
-  $('#add-buttons').click(function(event) {
-    $('.results').empty();
-  const random = responseJson.results[Math.floor(Math.random() * responseJson.results.length)];
-    $('.results').append(`
-      <img class="thumbnail" src="https://image.tmdb.org/t/p/original${random.poster_path}" />
-      <div id="overview">
-        <h2>Overview</h2>
-        <p>${random.overview}</p>
-      </div>
-      <div id="add-buttons">
-        <button class="add-btn btn-red btn-large" role="link">fetch tv show</button>
-        <button class="btn btn-red btn-large video-btn" role="link">Watch Trailer</button>
-      </div>
-    `)
-  $('.results').removeClass('hidden');
-  $('.video-btn').click(function(event) {
-    $('.results').append(`
-      <div class="video-container">
-        <iframe id="ytplayer" type="text/html" width="1200" height="800"
-        src="https://www.youtube.com/embed/?listType=search&list=${random.name} trailer"
-        frameborder="0" allowfullscreen>
-        </iframe>
-        <button class="close" role="link">X</button>
-      </div>
-    `)
+function getMovies(responseJson) {
+  $('.btn').on('click', function(event) {
+    const random = responseJson.results[Math.floor(Math.random() * responseJson.results.length)];
+    console.log(`"https://www.youtube.com/embed?listType=search&list=${random.name}"`)
+      $('.start').hide();
+      $('.results').append(`
+        <img class="thumbnail" src="https://image.tmdb.org/t/p/original${random.poster_path}" />
+        <div id="overview">
+          <h2>Overview</h2>
+          <p>${random.overview}</p>
+        </div>
+        <div id="add-buttons">
+          <button class="add-btn btn-red btn-large" role="link">fetch tv show</button>
+          <button class="btn btn-red btn-large video-btn" role="link">watch trailer</button>
+        </div>
+      `)
+    $('.results').removeClass('hidden');
+    $('.left-container').on('click', '.video-btn', function(event) {
+      $('.results').append(`
+        <div class="video-container">
+          <iframe id="ytplayer" type="text/html" width="1200" height="800"
+          src="https://www.youtube.com/embed/?listType=search&list=${random.name} trailer"
+          frameborder="0" allowfullscreen>
+          </iframe>
+          <button class="close" role="link">X</button>
+        </div>
+      `)
+      });
     });
-  });
+}
+
+
+function listenForShow(responseJson) {
+  getMovies(responseJson);
 };
+
 
 function closeButton(event) { 
   $('.results').on('click', '.close', function(event) {
@@ -71,17 +54,16 @@ function closeButton(event) {
   });
 };
 
-fetch(popularURL)
+fetch(url)
   .then(response => {
     if (response.ok) {
       return response.json();
     }
     throw new Error(response.statusText);
   }) 
-  .then(responseJson => displayResults(responseJson))
+  .then(displayResults)
   .catch(err => {
     $('#js-error-message').text(`Something went wrong: ${err.message}`);
   });
 
-$(fetchAnotherShow);
 $(closeButton);
